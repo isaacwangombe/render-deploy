8e5a4d3418199390781ed19ba8e4b5dc2b9785df
Isaac Wangombe <isaacwangombe5@gmail.com>
2022-11-29 08:56:28 +0300

    Deploying to railway

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 poetry install
-pip install --upgrade pippip install --force-reinstall -U setuptools
+# pip install --upgrade pippip install --force-reinstall -U setuptools
 
 
 python manage.py collectstatic --no-input
