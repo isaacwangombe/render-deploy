23a4bdf765a83e1e56cee46afb30506cd85e12a8
Isaac Wangombe <isaacwangombe5@gmail.com>
2022-12-01 12:50:40 +0300

    deployment

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+pip install --upgrade pippip install --force-reinstall -U setuptools
 
 poetry install
-pip install --upgrade pippip install --force-reinstall -U setuptools
 
 
 python manage.py collectstatic --no-input
