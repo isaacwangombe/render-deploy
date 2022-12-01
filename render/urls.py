d0e4840bd1351b5257319873532df6dea8669f46
Isaac Wangombe <isaacwangombe5@gmail.com>
2022-12-01 14:50:51 +0300

    deployment

--- render/urls.py
+++ render/urls.py
@@ -1,9 +1,9 @@
-# from django.urls import path, include
+from django.urls import path, include
 
-# from . import views
+from . import views
 
-# urlpatterns = [
-#     path('', views.index, name='port'),
-#     path('port/', views.index, name='port'),
+urlpatterns = [
+    # path('', views.index, name='port'),
+    # path('port/', views.index, name='port'),
 
-# ]
+]
